0e90e97de9bc8a6b60b648787e27abf3084724dd
Olival <olivalpaulinocc@gmail.com>
2023-05-03 07:12:50 -0300

    adicionando spring security e modificando para mysql

---
+++ src/main/java/com/api/parkingcontrol/security/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.api.parkingcontrol.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.httpBasic()
+		.and()
+		.authorizeHttpRequests()
+		.anyRequest().authenticated()
+		.and()
+		.csrf().disable();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("olivalpaulino")
+		.password("root")
+		.roles("ADMIN");
+	}
+}
